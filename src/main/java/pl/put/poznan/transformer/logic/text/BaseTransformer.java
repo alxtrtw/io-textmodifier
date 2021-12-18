1568b5c08b69fcf7d082f305f398deb309702b37
Hououin Kyouma <inwazzin@gmail.com>
2021-12-18 15:56:43 +0100

    base-transformer functionality

--- src/main/java/pl/put/poznan/transformer/logic/text/BaseTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/text/BaseTransformer.java
@@ -3,6 +3,6 @@ package pl.put.poznan.transformer.logic.text;
 public class BaseTransformer implements Transformer {
     @Override
     public String transform(String text) {
-        return text;
+        return text.strip();
     }
 }
