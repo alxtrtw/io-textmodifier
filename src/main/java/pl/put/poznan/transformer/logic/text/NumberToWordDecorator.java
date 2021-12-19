1e9b0cf4cef4596d4a000ce86434023bbdb50564
Alex Terentowicz <alext@man.poznan.pl>
2021-12-19 21:39:28 +0100

    Added translateInteger functionality to NumberToWordDecorator

--- src/main/java/pl/put/poznan/transformer/logic/text/NumberToWordDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/text/NumberToWordDecorator.java
@@ -1,11 +1,33 @@
 package pl.put.poznan.transformer.logic.text;
 
 import io.vavr.collection.Iterator;
-import pl.put.poznan.transformer.logic.PolishDictionary;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class NumberToWordDecorator extends TransformerDecorator {
+
+    List<String> units = Arrays.asList( "", "jeden", "dwa", "trzy", "cztery", "pięć",
+                                        "sześć", "siedem", "osiem", "dziewięć");
+
+    List<String> tens = Arrays.asList(  "", "jedenaście", "dwanaście", "trzynaście",
+                                        "czternaście", "piętnaście", "szesnaście",
+                                        "siedemnaście", "osiemnaście", "dziewiętnaście");
+
+    List<String> teens = Arrays.asList( "", "dziesięć", "dwadzieścia", "trzydzieści",
+                                        "czterdzieści", "pięćdziesiąt", "sześćdziesiąt",
+                                        "siedemdziesiąt", "osiemdziesiąt", "dziewięćdziesiąt");
+
+    List<String> hundreds  = Arrays.asList( "", "sto", "dwieście", "trzysta",
+                                            "czterysta", "pięćset", "sześćset",
+                                            "siedemset", "osiemset", "dziewięćset");
+
+    List<List<String>> groups = Arrays.asList(  Arrays.asList("", "", ""),
+                                                Arrays.asList("tysiąc", "tysiące", "tysięcy"));
+
     public NumberToWordDecorator(Transformer transformer) {
         super(transformer);
     }
@@ -16,35 +38,58 @@ public class NumberToWordDecorator extends TransformerDecorator {
     }
 
     private String translateInteger(String integerText) {
-        PolishDictionary dict = new PolishDictionary();
+        int number = Integer.parseInt(integerText);
 
-        String[] digits = integerText.split("");
-        String newText = "";
-
-        for (int i = 0; i < digits.length; i++) {
-            String digit = digits[i];
-            String newWord = "";
-
-            switch (digits.length - i) {
-                case 3:
-                    newWord = dict.getHundredDict().get(digit);
-                    break;
-                case 2:
-                    if (digit.equals("1")) {
-                        newWord = dict.getTenDict().get(digit.concat(digits[digits.length - 1]));
-                    } else {
-                        newWord = dict.getTenDict().get(digit);
-                    }
-                    break;
-                case 1:
-                    newWord = dict.getDict().get(digit);
-                    break;
-            }
-
-            newText = newText.concat(newWord);
+        List<String> words = new ArrayList<String>();
+        if (number < 0) {
+            words.add("minus");
+            number = -number;
         }
 
-        return newText.strip();
+        int group = 0;
+        while (number!=0) {
+            int hundred = number % 1_000 / 100;
+            int teen = number % 100 / 10;
+            int unit = number % 10;
+            number = number / 1_000;
+
+            // brak elementów do nazwania
+            if (hundred == 0 && teen == 0 && unit == 0 ){
+                group += 1;
+                continue;
+            }
+
+            // łączymy dziesiątki i jedności w -naście
+            int ten = 0;
+            if (teen == 1 && unit > 0){
+                ten = unit;
+                teen = 0;
+                unit = 0;
+            }
+
+            // wybór formy gramatycznej
+            int form = 0;
+            if (unit == 1 && hundred + teen + ten == 0) {
+                form = 0;
+            } else if ( 2 <= unit && unit <= 4 ) {
+                form = 1;
+            } else {
+                form = 2;
+            }
+
+            if (group == 0) {
+                words.addAll(Arrays.asList(hundreds.get(hundred), teens.get(teen), tens.get(ten), units.get(unit)));
+            } else {
+                words.addAll(Arrays.asList( "", groups.get(group).get(form)));
+            }
+            group += 1;
+        }
+
+        String str = "";
+        for (String word : words){
+            str = str.concat(word).concat(" ");
+        }
+        return str.strip();
     }
 
     private String translateDecimal(String decimalText) {
