bceaa0000890523f1408c4c0f735a59af68ac73e
Hououin Kyouma <inwazzin@gmail.com>
2021-12-19 22:15:35 +0100

    cr

--- src/main/java/pl/put/poznan/transformer/logic/text/NumberToWordDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/text/NumberToWordDecorator.java
@@ -1,33 +1,16 @@
 package pl.put.poznan.transformer.logic.text;
 
 import io.vavr.collection.Iterator;
+import io.vavr.collection.Stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class NumberToWordDecorator extends TransformerDecorator {
-
-    List<String> units = Arrays.asList( "", "jeden", "dwa", "trzy", "cztery", "pięć",
-                                        "sześć", "siedem", "osiem", "dziewięć");
-
-    List<String> tens = Arrays.asList(  "", "jedenaście", "dwanaście", "trzynaście",
-                                        "czternaście", "piętnaście", "szesnaście",
-                                        "siedemnaście", "osiemnaście", "dziewiętnaście");
-
-    List<String> teens = Arrays.asList( "", "dziesięć", "dwadzieścia", "trzydzieści",
-                                        "czterdzieści", "pięćdziesiąt", "sześćdziesiąt",
-                                        "siedemdziesiąt", "osiemdziesiąt", "dziewięćdziesiąt");
-
-    List<String> hundreds  = Arrays.asList( "", "sto", "dwieście", "trzysta",
-                                            "czterysta", "pięćset", "sześćset",
-                                            "siedemset", "osiemset", "dziewięćset");
-
-    List<List<String>> groups = Arrays.asList(  Arrays.asList("", "", ""),
-                                                Arrays.asList("tysiąc", "tysiące", "tysięcy"));
-
     public NumberToWordDecorator(Transformer transformer) {
         super(transformer);
     }
@@ -40,38 +23,38 @@ public class NumberToWordDecorator extends TransformerDecorator {
     private String translateInteger(String integerText) {
         int number = Integer.parseInt(integerText);
 
-        List<String> words = new ArrayList<String>();
+        List<String> words = new ArrayList<>();
         if (number < 0) {
             words.add("minus");
             number = -number;
         }
 
         int group = 0;
-        while (number!=0) {
+        while (number != 0) {
             int hundred = number % 1_000 / 100;
             int teen = number % 100 / 10;
             int unit = number % 10;
             number = number / 1_000;
 
             // brak elementów do nazwania
-            if (hundred == 0 && teen == 0 && unit == 0 ){
+            if (hundred == 0 && teen == 0 && unit == 0) {
                 group += 1;
                 continue;
             }
 
             // łączymy dziesiątki i jedności w -naście
             int ten = 0;
-            if (teen == 1 && unit > 0){
+            if (teen == 1 && unit > 0) {
                 ten = unit;
                 teen = 0;
                 unit = 0;
             }
 
             // wybór formy gramatycznej
-            int form = 0;
+            int form;
             if (unit == 1 && hundred + teen + ten == 0) {
                 form = 0;
-            } else if ( 2 <= unit && unit <= 4 ) {
+            } else if (2 <= unit && unit <= 4) {
                 form = 1;
             } else {
                 form = 2;
@@ -80,16 +63,12 @@ public class NumberToWordDecorator extends TransformerDecorator {
             if (group == 0) {
                 words.addAll(Arrays.asList(hundreds.get(hundred), teens.get(teen), tens.get(ten), units.get(unit)));
             } else {
-                words.addAll(Arrays.asList( "", groups.get(group).get(form)));
+                words.addAll(Arrays.asList("", groups.get(group).get(form)));
             }
             group += 1;
         }
 
-        String str = "";
-        for (String word : words){
-            str = str.concat(word).concat(" ");
-        }
-        return str.strip();
+        return words.stream().filter(s -> !s.isEmpty()).collect(Collectors.joining(" "));
     }
 
     private String translateDecimal(String decimalText) {
@@ -110,7 +89,7 @@ public class NumberToWordDecorator extends TransformerDecorator {
     }
 
     private enum NumberType {
-        Integer(Pattern.compile("\\d+")), Decimal(Pattern.compile("")), Invalid(Pattern.compile(""));
+        Integer(Pattern.compile("\\d+")), Decimal(Pattern.compile("\\d+(\\.\\d+)?")), Invalid(Pattern.compile(""));
 
         NumberType(Pattern pattern) {
             this.pattern = pattern;
@@ -124,5 +103,23 @@ public class NumberToWordDecorator extends TransformerDecorator {
             return Invalid;
         }
     }
+
+    private final List<String> units = Arrays.asList("", "jeden", "dwa", "trzy", "cztery", "pięć",
+        "sześć", "siedem", "osiem", "dziewięć");
+
+    private final List<String> tens = Arrays.asList("", "jedenaście", "dwanaście", "trzynaście",
+        "czternaście", "piętnaście", "szesnaście",
+        "siedemnaście", "osiemnaście", "dziewiętnaście");
+
+    private final List<String> teens = Arrays.asList("", "dziesięć", "dwadzieścia", "trzydzieści",
+        "czterdzieści", "pięćdziesiąt", "sześćdziesiąt",
+        "siedemdziesiąt", "osiemdziesiąt", "dziewięćdziesiąt");
+
+    private final List<String> hundreds = Arrays.asList("", "sto", "dwieście", "trzysta",
+        "czterysta", "pięćset", "sześćset",
+        "siedemset", "osiemset", "dziewięćset");
+
+    private final List<List<String>> groups = Arrays.asList(Arrays.asList("", "", ""),
+        Arrays.asList("tysiąc", "tysiące", "tysięcy"));
 }
 
