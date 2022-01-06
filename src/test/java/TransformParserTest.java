070e75e0f6c64685335dcca41826ce26531dc8f4
Alex Terentowicz <alext@man.poznan.pl>
2022-01-06 15:20:51 +0100

    Added TransformParser Tests

---
+++ src/test/java/TransformParserTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import pl.put.poznan.transformer.rest.TransformParser;
+import pl.put.poznan.transformer.rest.TransformMethod;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class TransformParserTest {
+
+    @Test
+    public void ShouldReturnTransforms() {
+        TransformParser decorator = mock(TransformParser.class);
+        List<String> input = Arrays.asList("id", "i", "u", "l", "c", "se", "sc", "ntw", "rr", "cc");
+        List<TransformMethod> expected =
+                Arrays.asList(  TransformMethod.Identity, TransformMethod.Inverse, TransformMethod.Uppercase,
+                                TransformMethod.Lowercase, TransformMethod.Capitalize, TransformMethod.ShortcutExpanse,
+                                TransformMethod.ShortcutCollapse, TransformMethod.NumberToWord,
+                                TransformMethod.RemoveRepeats, TransformMethod.CesarCipher);
+
+        assertEquals(expected, TransformParser.from(input));
+    }
+
+    @Test
+    public void ShouldReturnDuplicates() {
+        TransformParser decorator = mock(TransformParser.class);
+        List<String> input = Arrays.asList("cc", "cesar", "cesar-cipher");
+        List<TransformMethod> expected =
+                Arrays.asList(TransformMethod.CesarCipher, TransformMethod.CesarCipher, TransformMethod.CesarCipher);
+
+        assertEquals(expected, TransformParser.from(input));
+    }
+}
