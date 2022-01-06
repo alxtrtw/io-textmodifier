af9239418fa19b353886fed2c44b0db750b8fc19
Hououin Kyouma <inwazzin@gmail.com>
2022-01-06 14:16:09 +0100

    cleanup tests

---
+++ src/test/java/TransformationsTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+import pl.put.poznan.transformer.logic.text.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class TransformationsTest {
+    @Test
+    public void test_UppercaseTransformation() {
+        Transformer decorator = mock(UppercaseDecorator.class);
+        var input = "UPPERCASEDECORATOR";
+        var expected = "uppercasedecorator";
+
+        when(decorator.transform(input)).thenReturn(expected);
+
+        assertEquals(expected, decorator.transform(input));
+    }
+
+    @Test
+    public void test_LowercaseTransformation() {
+        Transformer transformer = new LowercaseDecorator(base);
+        var input = "LOWERCASEDECORATOR";
+        var expected = "lowercasedecorator";
+
+        assertEquals(expected, transformer.transform(input));
+    }
+
+    @Test
+    public void test_ShortcutExpandTransformation() {
+        Transformer transformer = new ShortcutExpanderDecorator(base);
+        var input = "Pan Prof. spóźnił się na zajęcia";
+        var expected = "Pan Profesor spóźnił się na zajęcia";
+
+        assertEquals(expected, transformer.transform(input));
+    }
+
+    @Test
+    public void test_ShortcutCollapseTransformation() {
+        Transformer transformer = new ShortcutCollapserDecorator(base);
+        var input = "Pieczywo to Na przykład na przykład  chleb i bułki";
+        var expected = "Pieczywo to np. np. chleb i bułki";
+
+        assertEquals(expected, transformer.transform(input));
+    }
+
+    @Test
+    public void test_InverseTransformation() {
+        Transformer transformer = new InverseDecorator(base);
+        var input = "MirEk";
+        var expected = "KerIm";
+
+        assertEquals(expected, transformer.transform(input));
+    }
+
+    private static final BaseTransformer base = new BaseTransformer();
+}
